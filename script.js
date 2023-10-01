c8a74e69ca1ca59682fcd83755e35d76f37700e6
Gyan <pgyan4224@gmail.com>
2023-10-01 20:24:53 +0530

    Got the error mind the 'px'. Rectifed by keshu bhai

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ let ogWidth = 50;
 elementImage.style.width = ogWidth + "px";
 displayWidth.textContent = ogWidth + "px";
 
+function onInc(){
+    if(ogWidth >= maxImageWidth){
+        ErrorMessage.textContent = "Reached the threshold, Won't look good on further increase!";
+    } else{
+        ogWidth = ogWidth + 5;
+        let newWidth = ogWidth;
+        elementImage.style.width = newWidth + "px";
+        displayWidth.textContent = newWidth + "px";
+        ErrorMessage.textContent = "";
+    }
+}
+
 function onDec(){
     if(ogWidth <= minImageWidth){
         ErrorMessage.textContent = "Can't be Resized further, Not visible with the naked human eyes";
-    }else{
-        elementImage.style.width = ogWidth - 5;
-        displayWidth.textContent = ogWidth - 5 + "px";
+    } else{
+        ogWidth = ogWidth - 5;
+        let DecWidth = ogWidth;
+        elementImage.style.width = DecWidth + "px";
+        displayWidth.textContent = DecWidth + "px";
         ErrorMessage.textContent = "";
     }
 }
